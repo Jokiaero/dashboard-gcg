1f3f2de77814b125d9b8ce77cdc2bde184ddb1e3
Samuel Simanjuntak <samuelsimanjuntak078@gmail.com>
2026-03-12 14:14:53 +0700

    chore: use prisma singleton pattern

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-    prismaInstance: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-    globalForPrisma.prismaInstance ??
-    new PrismaClient({
-        log: ["query"],
-    });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prismaInstance = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
